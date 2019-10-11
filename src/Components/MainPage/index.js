7fc0daaf0a29f3a8d626423568d7024ad6315892
gabrieltino <miracle8093@gmail.com>
2019-10-11 09:01:07 +0100

    ran npm audit fix --force

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import { Modal, ModalHeader, ModalBody, FormGroup, Label, NavbarBrand } from 'reactstrap';
+import React from "react";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+  NavbarBrand
+} from "reactstrap";
 
 const photos = [
-  { src: '/images/vict-baby.png' },
-  { src: '/images/ned.jpeg' },
-  { src: '/images/devilgirl.jpg' },
-  { src: '/images/trump.jpg' },
-  { src: '/images/one-does-not.jpg' },
-  { src: '/images/dank.png' },
-  { src: '/images/boy.png' },
-  { src: '/images/sad.png' },
-  { src: '/images/wolf.png' },
-  { src: '/images/fry.jpg' },
-  { src: '/images/jobs.jpg' },
-  { src: '/images/phone.jpg' },
-  { src: '/images/oldie.png' },
-  { src: '/images/image.png' },
-  { src: '/images/doubt.png' },
-  { src: '/images/crying.png' },
-  { src: '/images/sponge.png' },
-  { src: '/images/dog.png' },
-  { src: '/images/frust.png' },
-  { src: '/images/web.png' },
-  { src: '/images/penguin.png' }
+  { src: "/images/vict-baby.png" },
+  { src: "/images/ned.jpeg" },
+  { src: "/images/devilgirl.jpg" },
+  { src: "/images/trump.jpg" },
+  { src: "/images/one-does-not.jpg" },
+  { src: "/images/dank.png" },
+  { src: "/images/boy.png" },
+  { src: "/images/sad.png" },
+  { src: "/images/wolf.png" },
+  { src: "/images/fry.jpg" },
+  { src: "/images/jobs.jpg" },
+  { src: "/images/phone.jpg" },
+  { src: "/images/oldie.png" },
+  { src: "/images/image.png" },
+  { src: "/images/doubt.png" },
+  { src: "/images/crying.png" },
+  { src: "/images/sponge.png" },
+  { src: "/images/dog.png" },
+  { src: "/images/frust.png" },
+  { src: "/images/web.png" },
+  { src: "/images/penguin.png" }
 ];
 
 const initialState = {
@@ -34,7 +41,7 @@ const initialState = {
   topX: "50%",
   bottomX: "50%",
   bottomY: "90%"
-}
+};
 
 class MainPage extends React.Component {
   constructor() {
@@ -47,7 +54,7 @@ class MainPage extends React.Component {
     };
   }
 
-  openImage = (index) => {
+  openImage = index => {
     const image = photos[index];
     const base_image = new Image();
     base_image.src = image.src;
@@ -58,19 +65,19 @@ class MainPage extends React.Component {
       currentImagebase64: base64,
       ...initialState
     }));
-  }
+  };
 
   toggle = () => {
     this.setState(prevState => ({
       modalIsOpen: !prevState.modalIsOpen
     }));
-  }
+  };
 
-  changeText = (event) => {
+  changeText = event => {
     this.setState({
       [event.currentTarget.name]: event.currentTarget.value
     });
-  }
+  };
 
   getStateObj = (e, type) => {
     let rect = this.imageRef.getBoundingClientRect();
@@ -83,32 +90,34 @@ class MainPage extends React.Component {
         isTopDragging: false,
         bottomX: `${xOffset}px`,
         bottomY: `${yOffset}px`
-      }
+      };
     } else if (type === "top") {
       stateObj = {
         isTopDragging: true,
         isBottomDragging: false,
         topX: `${xOffset}px`,
         topY: `${yOffset}px`
-      }
+      };
     }
     return stateObj;
-  }
+  };
 
   handleMouseDown = (e, type) => {
     const stateObj = this.getStateObj(e, type);
-    document.addEventListener('mousemove', (event) => this.handleMouseMove(event, type));
+    document.addEventListener("mousemove", event =>
+      this.handleMouseMove(event, type)
+    );
     this.setState({
       ...stateObj
-    })
-  }
+    });
+  };
 
   handleMouseMove = (e, type) => {
     if (this.state.isTopDragging || this.state.isBottomDragging) {
       let stateObj = {};
       if (type === "bottom" && this.state.isBottomDragging) {
         stateObj = this.getStateObj(e, type);
-      } else if (type === "top" && this.state.isTopDragging){
+      } else if (type === "top" && this.state.isTopDragging) {
         stateObj = this.getStateObj(e, type);
       }
       this.setState({
@@ -117,13 +126,13 @@ class MainPage extends React.Component {
     }
   };
 
-  handleMouseUp = (event) => {
-    document.removeEventListener('mousemove', this.handleMouseMove);
+  handleMouseUp = event => {
+    document.removeEventListener("mousemove", this.handleMouseMove);
     this.setState({
       isTopDragging: false,
       isBottomDragging: false
     });
-  }
+  };
 
   convertSvgToImage = () => {
     const svg = this.svgRef;
@@ -134,7 +143,10 @@ class MainPage extends React.Component {
     canvas.width = svgSize.width;
     canvas.height = svgSize.height;
     const img = document.createElement("img");
-    img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))));
+    img.setAttribute(
+      "src",
+      "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData)))
+    );
     img.onload = function() {
       canvas.getContext("2d").drawImage(img, 0, 0);
       const canvasdata = canvas.toDataURL("image/png");
@@ -144,7 +156,7 @@ class MainPage extends React.Component {
       document.body.appendChild(a);
       a.click();
     };
-  }
+  };
 
   getBase64Image(img) {
     var canvas = document.createElement("canvas");
@@ -170,7 +182,7 @@ class MainPage extends React.Component {
       fill: "#FFF",
       stroke: "#000",
       userSelect: "none"
-    }
+    };
 
     return (
       <div>
@@ -178,10 +190,8 @@ class MainPage extends React.Component {
           <div className="sidebar">
             <NavbarBrand href="/">Make-a-Meme</NavbarBrand>
             <p>
-              This is a fun 5 hour project inspired by imgur. Built with React.
-            </p>
-            <p>
-              You can add top and bottom text to a meme-template, move the text around and can save the image by downloading it.
+              You can add top and bottom text to a meme-template, move the text
+              around and can save the image by downloading it.
             </p>
           </div>
           <div className="content">
@@ -199,7 +209,7 @@ class MainPage extends React.Component {
                   onClick={() => this.openImage(index)}
                   role="presentation"
                 />
-              <span className="meme-bottom-caption">Bottom text</span>
+                <span className="meme-bottom-caption">Bottom text</span>
               </div>
             ))}
           </div>
@@ -211,25 +221,33 @@ class MainPage extends React.Component {
               width={newWidth}
               id="svg_ref"
               height={newHeight}
-              ref={el => { this.svgRef = el }}
+              ref={el => {
+                this.svgRef = el;
+              }}
               xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink">
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
               <image
-                ref={el => { this.imageRef = el }}
+                ref={el => {
+                  this.imageRef = el;
+                }}
                 xlinkHref={this.state.currentImagebase64}
                 height={newHeight}
                 width={newWidth}
               />
               <text
-                style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
+                style={{
+                  ...textStyle,
+                  zIndex: this.state.isTopDragging ? 4 : 1
+                }}
                 x={this.state.topX}
                 y={this.state.topY}
                 dominantBaseline="middle"
                 textAnchor="middle"
-                onMouseDown={event => this.handleMouseDown(event, 'top')}
-                onMouseUp={event => this.handleMouseUp(event, 'top')}
+                onMouseDown={event => this.handleMouseDown(event, "top")}
+                onMouseUp={event => this.handleMouseUp(event, "top")}
               >
-                  {this.state.toptext}
+                {this.state.toptext}
               </text>
               <text
                 style={textStyle}
@@ -237,27 +255,46 @@ class MainPage extends React.Component {
                 textAnchor="middle"
                 x={this.state.bottomX}
                 y={this.state.bottomY}
-                onMouseDown={event => this.handleMouseDown(event, 'bottom')}
-                onMouseUp={event => this.handleMouseUp(event, 'bottom')}
+                onMouseDown={event => this.handleMouseDown(event, "bottom")}
+                onMouseUp={event => this.handleMouseUp(event, "bottom")}
               >
-                  {this.state.bottomtext}
+                {this.state.bottomtext}
               </text>
             </svg>
             <div className="meme-form">
               <FormGroup>
                 <Label for="toptext">Top Text</Label>
-                <input className="form-control" type="text" name="toptext" id="toptext" placeholder="Add text to the top" onChange={this.changeText} />
+                <input
+                  className="form-control"
+                  type="text"
+                  name="toptext"
+                  id="toptext"
+                  placeholder="Add text to the top"
+                  onChange={this.changeText}
+                />
               </FormGroup>
               <FormGroup>
                 <Label for="bottomtext">Bottom Text</Label>
-                <input className="form-control" type="text" name="bottomtext" id="bottomtext" placeholder="Add text to the bottom" onChange={this.changeText} />
+                <input
+                  className="form-control"
+                  type="text"
+                  name="bottomtext"
+                  id="bottomtext"
+                  placeholder="Add text to the bottom"
+                  onChange={this.changeText}
+                />
               </FormGroup>
-              <button onClick={() => this.convertSvgToImage()} className="btn btn-primary">Download Meme!</button>
+              <button
+                onClick={() => this.convertSvgToImage()}
+                className="btn btn-primary"
+              >
+                Download Meme!
+              </button>
             </div>
           </ModalBody>
         </Modal>
       </div>
-    )
+    );
   }
 }
 
